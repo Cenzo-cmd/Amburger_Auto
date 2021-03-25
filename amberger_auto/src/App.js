43a68b8a8a0a728dbc39328269e10aa14ede952b
Vincent <vrdphone@gmail.com>
2021-03-25 16:01:35 -0500

    adding files

---
+++ amberger_auto/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
